50459dbbbdd11081a7e341e0125589c9ccb64314
onotaka1122 <onotakahiro1122@gmail.com>
2016-11-18 11:27:34 +0800

    Initial Commit

---
+++ myAprication004/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  myAprication004
+//
+//  Created by Apple on 2016/11/18.
+//  Copyright © 2016年 Takahiro Ono. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
