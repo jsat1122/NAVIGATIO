aaf92cc5da3b2a7694317fa8effd71619096ef62
onotaka1122 <onotakahiro1122@gmail.com>
2016-12-05 23:52:34 +0800

    日記の編集

--- myAprication004/ReadDiaryViewController.swift
+++ myAprication004/ReadDiaryViewController.swift
@@ -83,8 +83,7 @@ class ReadDiaryViewController: UIViewController {
     //削除ボタンが押された時に呼ばれるメソッド
     func myDelete(){
         print("削除")
-//        var targetView: AnyObject = self.storyboard!.instantiateViewController(withIdentifier: "MainViewController")
-//        self.present(targetView as! UIViewController, animated: true, completion: nil)
+        navigationController?.viewControllers.removeLast() //views to pop
     }
 
     override func didReceiveMemoryWarning() {
@@ -124,5 +123,8 @@ class ReadDiaryViewController: UIViewController {
 //        self.myWebView.reload()
 //    }
 
+    @IBAction func editBtn(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
